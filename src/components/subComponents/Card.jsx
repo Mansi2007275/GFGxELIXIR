ff61225952de58a4dfbbb69a4cf16c37851aad01
Dheershikha123 <dheerjanmejay6@gmail.com>
2026-02-11 16:04:50 +0530

    updated font

--- src/components/subComponents/Card.jsx
+++ src/components/subComponents/Card.jsx
@@ -37,7 +37,7 @@ const cardData = [
   },
   {
     icon: <Lightbulb />,
-    title: "Innovation",
+    title: "Open Innovation",
     desc: "Creative Nebula",
     number: "04",
     tagline: "Innovate without boundaries",
